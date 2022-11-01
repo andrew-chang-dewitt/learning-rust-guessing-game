34c7890045d129db9aaf202ea754a73491d2ad6c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2022-11-01 11:57:11 -0500

    Better doc comments & fix typo.

--- src/random.rs
+++ src/random.rs
@@ -2,7 +2,6 @@ use rand::{rngs::ThreadRng, Rng};
 
 use crate::constants::{MAX_SECRET, MIN_SECRET};
 
-/// Generate a secret number between MIN_SECRET & MAX_SECRET
 pub struct NumberGenerator {
     thread_rng: Option<ThreadRng>,
     max: usize,
@@ -10,6 +9,8 @@ pub struct NumberGenerator {
 }
 
 impl NumberGenerator {
+    /// Create a number generator based on ThreadRng & gen_range with given
+    /// min & max values
     pub fn new(min: usize, max: usize) -> Self {
         NumberGenerator {
             thread_rng: None,
@@ -18,6 +19,7 @@ impl NumberGenerator {
         }
     }
 
+    /// Generate a secret number
     pub fn gen_secret(&mut self) -> usize {
         self.get_rng().gen_range(&self.min, &self.max)
     }
@@ -36,8 +38,9 @@ impl NumberGenerator {
 }
 
 impl Default for NumberGenerator {
+    /// Create a Number Generator with MIN & MAX values from crate::constants
     fn default() -> Self {
-        Self::new(MIN_SECRET, MIN_SECRET)
+        Self::new(MIN_SECRET, MAX_SECRET)
     }
 }
 
